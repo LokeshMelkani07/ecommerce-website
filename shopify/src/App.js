778be1f46709138e249071722b799d05d5fca2f7
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-03-04 12:26:38 +0530

    React-router dom updated

--- shopify/src/App.js
+++ shopify/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+import SingleProduct from "./components/SingleProduct";
+import Cart from "./components/Cart";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is our app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
